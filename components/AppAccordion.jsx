57f40f00c8e06941b0a77ec122f53ef76abff9f8
mbravosarria <mbravosarria@gmail.com>
2022-10-06 15:37:41 -0400

    feat: help view

---
+++ components/AppAccordion.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Accordion, AccordionSummary, AccordionDetails, Divider } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import PropTypes from 'prop-types'
+
+function AppAccordion({ title, text }) {
+  return (
+    <>
+      <Accordion className='bg-background-300 shadow-xl'>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <div className='text-footer-background-200 font-semibold text-lg'>
+            {title}
+          </div>
+        </AccordionSummary>
+        <Divider />
+        <AccordionDetails>
+          <div className='text-footer-background-200 font-semibold'>
+            {text}
+          </div>
+        </AccordionDetails>
+      </Accordion>
+    </>
+  )
+}
+
+AppAccordion.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string
+}
+
+export default AppAccordion
--- pages/help.jsx
+++ pages/help.jsx
@@ -1,15 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import MainLayout from '../layouts/MainLayout'
 import AppHeader from '../components/layouts/AppHeader'
+import AppAccordion from '../components/AppAccordion'
 
 function Help() {
+  const questions = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
   const { t } = useTranslation()
+  const [view, setView] = useState('faq')
+
+  const handleChangeView = (option) => {
+    if (option !== view) {
+      setView(option)
+    }
+  }
 
   return (
     <>
      <AppHeader title={t('pages.help')}/>
-     <div className='flex flex-col my-8 mx-4 lg:mx-40'></div>
+     <div className='flex flex-col my-8 mx-4 lg:mx-40'>
+      <div className='flex flex-row justify-around mb-8'>
+        <div
+          className='w-5/12 text-center border py-2 rounded-xl hover:cursor-pointer hover:shadow-lg'
+          onClick={() => { handleChangeView('faq') }}
+        >
+          {t('help.questionTitle')}
+        </div>
+        <div
+          className='w-5/12 text-center border py-2 rounded-xl hover:cursor-pointer hover:shadow-lg'
+          onClick={() => { handleChangeView('how') }}
+        >
+          {t('help.how.title')}
+        </div>
+      </div>
+      {view === 'faq' &&
+        questions.map((question) => (
+          <AppAccordion
+            key={question}
+            title={t(`help.questions.${question}.question`)}
+            text={t(`help.questions.${question}.answer`)}
+          />
+        ))
+      }
+      {view === 'how' &&
+        <div className='w-full text-center text-[7rem]'>HOW TO USE VIEW</div>
+      }
+     </div>
     </>
   )
 }
