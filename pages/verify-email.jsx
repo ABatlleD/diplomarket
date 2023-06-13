68dddb7aaace15e6ad57a9c5de93d86f2473736b
Adrian Batlle Diego <adrianbatllediego@gmail.com>
2023-06-13 14:16:21 -0400

    dar margen a los mensajes de usuario cambiar icon de mensaje de usuario pago directo

--- pages/verify-email.jsx
+++ pages/verify-email.jsx
@@ -2,19 +2,16 @@ import React from "react"
 import Image from "next/image"
 import { Button } from "@mui/material"
 import Router from "next/router"
-import { useCart } from "../store/cart/cart.context"
 import { useTranslation } from "react-i18next"
 import VerifyEmailImg from "../public/assets/user-message/2165310.png"
 import dynamic from "next/dynamic"
 
 const MainLayout = dynamic(() => import("../layouts/MainLayout"))
 
-const PaymentError = () => {
+const VerifyEmail = () => {
     const { t } = useTranslation()
-    const { resetCart } = useCart()
-    resetCart()
     return (
-        <div className="flex justify-center py-10">
+        <div className="flex justify-center py-10 mx-5">
             <div className="container max-w-sm">
                 <div className="flex flex-wrap justify-center">
                     <div className="mb-3 text-center relative w-28 h-28">
@@ -58,8 +55,8 @@ const PaymentError = () => {
     )
 }
 
-PaymentError.getLayout = function getLayout(page) {
+VerifyEmail.getLayout = function getLayout(page) {
     return <MainLayout pageProps={page}>{page}</MainLayout>
 }
 
-export default PaymentError
+export default VerifyEmail
